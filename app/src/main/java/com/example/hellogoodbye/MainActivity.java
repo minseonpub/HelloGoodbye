2acf4f1538d75aeb0732351f9517ee2ea39218d2
minseon <minseonpub@gmail.com>
2020-04-23 17:28:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/hellogoodbye/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.hellogoodbye;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick1(View v)
+    {
+        ConstraintLayout container = (ConstraintLayout) findViewById(R.id.container);
+        container.setBackgroundResource(R.drawable.exclamationbtn);
+    }
+
+    public void onClick2(View v)
+    {
+        ConstraintLayout container = (ConstraintLayout) findViewById(R.id.container);
+        container.setBackgroundResource(R.drawable.greetimg);
+    }
+
+    public void onClick3(View v)
+    {
+        ConstraintLayout container = (ConstraintLayout) findViewById(R.id.container);
+        container.setBackgroundResource(R.drawable.myself);
+    }
+}
+
